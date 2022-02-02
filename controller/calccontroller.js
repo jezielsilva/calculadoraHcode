d39063a73b92bb76ddca23bd19045b77e537a61d
Jeziel Silva <silvajeziel22@gmail.com>
2022-02-02 19:05:51 -0300

    calculadora js do curso Hcode

---
+++ controller/calccontroller.js
@@ -0,0 +1,71 @@
+// JavaScript Document
+class CalcController{
+	constructor(){
+		this._locale = 'pt-BR';
+		this._displayCalcEl=document.querySelector("#display");
+		this._dateEl = document.querySelector("#data");
+		this._timeEl = document.querySelector("#hora");
+		this._currentDate;
+		this.initialize();
+		this.initButtonsEvents();
+	}
+	initialize(){
+		this.setDisplayTime();
+		setInterval(()=>{
+			this.setDisplayTime();
+		},1000);
+		
+	}
+	
+	addEventListenerAll(element, events, fn){
+		events.split(' ').forEach(event =>{
+			element.addEventListener(event, fn, false);
+		});
+	}
+	
+	initButtonsEvents(){
+		let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+		buttons.forEach((btn,index)=>{
+			this.addEventListenerAll(btn, "click drag", e=>{
+			console.log(btn.className.baseVal.replace("btn-", " "));
+			});
+			
+			this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+				btn.style.cursor = "pointer";
+			});
+		});
+	}
+	
+	setDisplayTime(){
+		this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+		this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+	}
+	get displayTime(){
+		return this._timeEl.innerHTML;
+	}
+	
+	set displayTime(value){
+		return this._timeEl.innerHTML = value;
+	}
+	
+	get displayDate (){
+		return this._dateEl.innerHTML;
+	}
+	
+	set displayDate (value){
+		return this._dateEl.innerHTML = value;
+	}
+	
+	get displayCalc(){
+		return this._displayCalcEl.innerHTML;
+	}
+	set displayCalc(value){
+		this._displayCalcEl.innerHTML = value;
+	}
+	get currentDate(){
+		return new Date();
+	}
+	set currentDate(value){
+		this._currentDate = value;
+	}
+}
